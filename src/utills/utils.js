e113dc5d8f2f368eb757bee79b5ce2448d958e11
yhsol <yhsol1592@gmail.com>
2019-07-27 20:00:45 +0900

    add request login secret

---
+++ src/utills/utils.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+import jwt from "jsonwebtoken";
+
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
