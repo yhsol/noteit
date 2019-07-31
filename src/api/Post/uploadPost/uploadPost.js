6ade8f6cb1159ffd8f7330887a98f4a1bc77397d
yhsol <yhsol1592@gmail.com>
2019-07-31 21:23:41 +0900

    add upload post

---
+++ src/api/Post/uploadPost/uploadPost.js
@@ -0,0 +1,26 @@
+import { isAuthenticated } from "../../../../middleware";
+import { prisma } from "../../../../generated/prisma-client";
+
+// TODO: files 들은 어떻게 올려야되지?
+
+export default {
+  Mutation: {
+    addPost: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const { text, title, tags, location } = args;
+      const postContent = await prisma.createPost({
+        user: {
+          connect: {
+            id: user.id
+          }
+        },
+        text,
+        title,
+        tags,
+        location
+      });
+      return postContent;
+    }
+  }
+};
