9a58f7a96a6d4c52efbe9228efd494e49c114512
yhsol <yhsol1592@gmail.com>
2019-08-02 21:30:15 +0900

    add follow/unfollow

---
+++ src/api/User/Follow/follow.js
@@ -0,0 +1,27 @@
+import { isAuthenticated } from "../../../../middleware";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    follow: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const { id } = args;
+      try {
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: {
+            following: {
+              connect: {
+                id
+              }
+            }
+          }
+        });
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
+  }
+};
