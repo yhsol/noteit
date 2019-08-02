09bcbf1f43ef11fbdc1bcbf034da498ff6d2d7db
yhsol <yhsol1592@gmail.com>
2019-08-02 21:31:29 +0900

    add edit user

---
+++ src/api/User/EditUser/editUser.js
@@ -0,0 +1,16 @@
+import { isAuthenticated } from "../../../../middleware";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    editUser: (_, args, { request }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const { email, username, firstName, lastName, bio } = args;
+      return prisma.updateUser({
+        where: { id: user.id },
+        data: { email, username, firstName, lastName, bio }
+      });
+    }
+  }
+};
