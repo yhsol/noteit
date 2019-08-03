35e1075d843d4450952370408dffea6dc0c34c86
yhsol <yhsol1592@gmail.com>
2019-08-03 17:53:28 +0900

    add me section

---
+++ src/api/User/me/me.js
@@ -0,0 +1,12 @@
+import { isAuthenticated } from "../../../../middleware";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    me: async (_, __, { request }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      return prisma.user({ id: user.id });
+    }
+  }
+};
