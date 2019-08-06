6986a0ad603ce318b06408aa0b590b49419ab42e
yhsol <yhsol1592@gmail.com>
2019-08-06 21:02:07 +0900

    add computed file

---
+++ src/api/User/userComputed.js
@@ -0,0 +1,45 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    post: ({ id }) => prisma.user({ id }).posts(),
+    following: ({ id }) => prisma.user({ id }).following(),
+    followers: ({ id }) => prisma.user({ id }).followers(),
+    like: ({ id }) => prisma.user({ id }).like(),
+    comments: ({ id }) => prisma.user({ id }).comments(),
+    room: ({ id }) => prisma.user({ id }).rooms(),
+    postCount: ({ id }) =>
+      prisma
+        .postsConnection({ where: { user: { id } } })
+        .aggregate()
+        .count(),
+    followingCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { followers_some: { id } } })
+        .aggregate()
+        .count(),
+    followersCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { following_some: { id } } })
+        .aggregate()
+        .count(),
+    fullName: parent => `${parent.firstName} ${parent.lastName}`,
+    isFollowing: async (parent, __, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return await prisma.$exists.user({
+          AND: [{ id: user.id }, { id: parentId }]
+        });
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    itsMe: (parent, __, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  }
+};
