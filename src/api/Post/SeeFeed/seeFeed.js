e384c1dab38e708e7850b978f61329588ecd1ed6
yhsol <yhsol1592@gmail.com>
2019-08-10 19:56:49 +0900

    add see feed

--- src/api/Post/SeeFeed/seeFeed.js
+++ src/api/Post/SeeFeed/seeFeed.js
@@ -7,6 +7,12 @@ export default {
       isAuthenticated(request);
       const { user } = request;
       const following = await prisma.user({ id: user.id }).following();
+      console.log(following);
+      if (following) {
+        //   show following feed
+      } else {
+        //  show sort feed(like recent, like, view, comment)
+      }
     }
   }
 };
