28ad590c223e65be7cd9c813c69b5e5fabe933c2
yhsol <yhsol1592@gmail.com>
2019-08-11 14:00:16 +0900

    add see feed section

--- src/api/Post/SeeFeed/seeFeed.js
+++ src/api/Post/SeeFeed/seeFeed.js
@@ -3,16 +3,16 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    seeFeed: async (_, args, { request }) => {
+    seeFeed: async (_, __, { request }) => {
       isAuthenticated(request);
       const { user } = request;
       const following = await prisma.user({ id: user.id }).following();
-      console.log(following);
-      if (following) {
-        //   show following feed
-      } else {
-        //  show sort feed(like recent, like, view, comment)
-      }
+      return prisma.posts({
+        where: {
+          user: { id_in: [...following.map(user => user.id), user.id] }
+        },
+        orderBy: "createdAt_DESC"
+      });
     }
   }
 };
