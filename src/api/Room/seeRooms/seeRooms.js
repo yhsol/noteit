e953e1f950896a41b307a3d3084ae86e46cc9eb3
yhsol <yhsol1592@gmail.com>
2019-08-13 22:28:44 +0900

    add room and message section

---
+++ src/api/Room/seeRooms/seeRooms.js
@@ -0,0 +1,15 @@
+import { isAuthenticated } from "../../../../middleware";
+import { prisma } from "../../../../generated/prisma-client";
+import { ROOM_FRAGMENT } from "../../../../fragment";
+
+export default {
+  Query: {
+    seeRooms: (_, __, { request }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      return prisma
+        .rooms({ where: { participants_some: { id: user.id } } })
+        .$fragment(ROOM_FRAGMENT);
+    }
+  }
+};
