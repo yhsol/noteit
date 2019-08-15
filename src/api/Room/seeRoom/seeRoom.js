870045e09871c7e5b7d86a0efd3b3a2fa2ff0000
yhsol <yhsol1592@gmail.com>
2019-08-15 14:27:27 +0900

    add see room section

---
+++ src/api/Room/seeRoom/seeRoom.js
@@ -0,0 +1,21 @@
+import { isAuthenticated } from "../../../../middleware";
+import { prisma } from "../../../../generated/prisma-client";
+import { ROOM_FRAGMENT } from "../../../../fragment";
+
+export default {
+  Query: {
+    seeRoom: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const { id } = args;
+      const canSeeRoom = await prisma.$exists.room({
+        participants_some: { id: user.id }
+      });
+      if (canSeeRoom) {
+        return prisma.room({ id }).$fragment(ROOM_FRAGMENT);
+      } else {
+        throw Error("You did not Authenticated");
+      }
+    }
+  }
+};
