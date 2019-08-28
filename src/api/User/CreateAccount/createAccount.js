1d00bf321288c1bf2e67c8a7439766ccb5011d6d
yhsol <yhsol1592@gmail.com>
2019-08-28 20:03:11 +0900

    add create account

--- src/api/User/CreateAccount/createAccount.js
+++ src/api/User/CreateAccount/createAccount.js
@@ -4,14 +4,14 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { email = "", username = "", firstName, lastName, bio } = args;
-      const user = await prisma.createUser({
-        email,
+      await prisma.createUser({
         username,
+        email,
         firstName,
         lastName,
         bio
       });
-      return user;
+      return true;
     }
   }
 };
