8f452019f740ddb8c34a06acf7b4787e0bb33049
yhsol <yhsol1592@gmail.com>
2019-10-09 19:51:10 +0900

    add ;

--- src/schema.js
+++ src/schema.js
@@ -6,8 +6,8 @@ const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
 const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
 
 const schema = makeExecutableSchema({
-    typeDefs: mergeTypes(allTypes),
-    resolvers: mergeResolvers(allResolvers)
-})
+  typeDefs: mergeTypes(allTypes),
+  resolvers: mergeResolvers(allResolvers)
+});
 
 export default schema;
